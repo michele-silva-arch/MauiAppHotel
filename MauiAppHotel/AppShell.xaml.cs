bc47d43c439be9f809a71e40d2fdcc23f4ae11fe
alexlimabr <alexsilval2000@gmail.com>
2025-05-29 21:05:58 -0300

    adicionar recursos ao aplicativo

--- MauiAppHotel/AppShell.xaml.cs
+++ MauiAppHotel/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Views;
+
+namespace MauiAppHotel
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(Sobre), typeof(Sobre));
         }
     }
 }
